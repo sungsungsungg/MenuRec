99e4a1602d03dc6bcf1a51fb1bef566f27be5c96
Sunghoon Kang <sunghoon@Sunghoons-MacBook-Air.local>
2025-03-28 21:15:08 -0400

    try fixing the directory problem again

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'src/dist', // This specifies that the build output should be in the root 'dist' folder
+  }
+
 })
