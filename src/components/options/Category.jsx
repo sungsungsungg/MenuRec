4b48eefbfe5006a61d889a713aa03a134995ec08
Sunghoon Kang <sunghoon@Sunghoons-Air.cable.rcn.com>
2025-03-29 22:56:15 -0400

    Remove unused categories

--- src/components/options/Category.jsx
+++ src/components/options/Category.jsx
@@ -10,15 +10,15 @@ function categoryDrop({onChange}){
             <label htmlFor="categories">Choose a category:&nbsp;</label>
             <select name="category" id="categories" className="dropdown" onChange={onChange}>
                 <option value="">None</option>
-                <option value="chinese">Chinese</option>
+                {/* <option value="chinese">Chinese</option> */}
                 <option value="korean">Korean</option>
-                <option value="italian">Italian</option>
-                <option value="japanese">Japanese</option>
-                <option value="sandwich">Sandwich</option>
-                <option value="ice cream">Ice Cream</option>
-                <option value="pizza">Pizza</option>
+                {/* <option value="italian">Italian</option> */}
+                {/* <option value="japanese">Japanese</option> */}
+                {/* <option value="sandwich">Sandwich</option> */}
+                {/* <option value="ice cream">Ice Cream</option> */}
+                {/* <option value="pizza">Pizza</option> */}
                 <option value="asian">Asian</option>
-                <option value="mexican">Mexican</option>
+                {/* <option value="mexican">Mexican</option> */}
             </select>
         </div>
     )
