0fefcff0f5a133a662699377933dd3fa512c0ca7
Sunghoon Kang <sunghoon@Sunghoons-Air.cable.rcn.com>
2025-03-30 00:37:05 -0400

    Delete Error tracing lines

--- src/components/pages/search.jsx
+++ src/components/pages/search.jsx
@@ -41,12 +41,12 @@ function SearchPage({ recData, handleSubmit, handleChange, coordinates, selected
 
   useEffect(()=>{
     if(selectedAddress.locality){
-      console.log("update cookie: ",selectedAddress);
+      // console.log("update cookie: ",selectedAddress);
       setCookieAddress(selectedAddress);
       setAddress(selectedAddress);
     }else if(getCookieAddress()){
       setAddress(getCookieAddress());
-      console.log("cookie address: ",getCookieAddress());
+      // console.log("cookie address: ",getCookieAddress());
       setSelectedAddress(getCookieAddress());
     }
   },[])
@@ -58,7 +58,7 @@ function SearchPage({ recData, handleSubmit, handleChange, coordinates, selected
     if(selectedAddress.locality){
       setCookieAddress(selectedAddress);
       setAddress(selectedAddress);
-      console.log("update cookie2: ",selectedAddress);
+      // console.log("update cookie2: ",selectedAddress);
     }
   },[selectedAddress]);
 
