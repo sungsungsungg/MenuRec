f960ca8d7e851462bc8020dceae077429346a865
Sunghoon Kang <sunghoon@Sunghoons-Air.cable.rcn.com>
2025-04-02 00:20:36 -0400

    Add page numbers for menu list

--- src/components/pages/search.jsx
+++ src/components/pages/search.jsx
@@ -80,15 +80,84 @@ function SearchPage({ recData, handleSubmit, handleChange, coordinates, selected
     />
   );
 
+  const createPage = ()=>{
+    let firstPage = Math.floor(menuPage/10)*10 +1;
+    let lastPage = (firstPage+9<recData.item.length/10)?firstPage+9:Math.floor(recData.item.length/10+1);
+    let pageList = [];
+    for(let i=firstPage;i<=lastPage;i++){
+      if((i===firstPage&&firstPage/10>=1)){
+        pageList.push("..");
+      }
+      pageList.push(i);
+      if((i)%10===0 && lastPage<recData.item.length/10){
+        pageList.push("...");
+      }
+    }
+    return pageList;
+  };
+
+  const createPageNumber =(number)=>{
+    if(Number.isInteger(number)){
+
+      if(number===menuPage+1){
+        return (
+          <button
+            key={number}
+            className ="page-number pageButton"
+            value={number}
+            onClick={()=> setPage(number-1)}
+            style={{color:"#EF9651"}}
+          >
+            {number}
+          </button>
+        )
+      }
+
+      return (<button
+        key={number}
+        className ="page-number pageButton"
+        value={number}
+        onClick={()=> setPage(number-1)}
+      >
+        {number}
+      </button>)
+      
+    }else{
+      let newNumber=Math.floor(menuPage/10)*10+10;
+      if(number==="..") newNumber = Math.floor(menuPage/10)*10-1;
+      return (
+        <button
+          key={number}
+          value={newNumber}
+          className = "page-number pageButton"
+          onClick={()=>setPage(newNumber)}
+        >
+          {number}
+        </button>
+      )
+      
+    }
+  }
+
+  
+  function setPage(page){
+    setMenuPage(page);
+  }
+
+  function firstPage(){
+    setMenuPage(0);
+  }
+
   function previousPage(){
     if(menuPage>0){
       setMenuPage((prev)=>prev-1);
     }
   }
 
-  function setPage(page){
-    setMenuPage(page);
+  function lastPage(){
+    setMenuPage(Math.floor(recData.item.length/10));
   }
+
 
   function nextPage(){
     if(menuPage<Math.floor(recData.item.length/10)){
@@ -111,20 +180,35 @@ function SearchPage({ recData, handleSubmit, handleChange, coordinates, selected
           <button type="submit">Apply</button>
         </form>
 
+        <div style={{display:'flex', flexDirection: 'column'}}>
+          <div className="container-menu">
+          {recData.item?(recData.item.length>0?<button className="pageButton" onClick={firstPage}> &lt;&lt;</button>:null):null}
+            {recData.item?(recData.item.length>0?<button className="pageButton" onClick={previousPage}> &lt;</button>:null):null}
+            {recData.item?(recData.item.length>0?createPage().map(createPageNumber):null):null}
+            {recData.item?(recData.item.length>0?<button className= "pageButton" onClick={nextPage}>&gt;</button>:null):null}
+            {recData.item?(recData.item.length>0?<button className= "pageButton" onClick={lastPage}>&gt;&gt;</button>:null):null}
+            
+          </div>
+          <br/>
+            {recData.item?(recData.item.length>0? <SortBy onChange={handleChange}/>:null):null}
+          <br/>
+            <div>
+            {recData.item ? (recData.item.length>0? recData.item.slice(menuPage*10,menuPage*10+10).map(createList):"No list available") : "Select your option"}
+            </div>
+            <div className="container-menu" style={{margin: "3em"}}>
+            {recData.item?(recData.item.length>0?<button className="pageButton" onClick={firstPage}> &lt;&lt;</button>:null):null}
+            {recData.item?(recData.item.length>0?<button className="pageButton" onClick={previousPage}> &lt;</button>:null):null}
+            {recData.item?(recData.item.length>0?createPage().map(createPageNumber):null):null}
+            {recData.item?(recData.item.length>0?<button className= "pageButton" onClick={nextPage}>&gt;</button>:null):null}
+            {recData.item?(recData.item.length>0?<button className= "pageButton" onClick={lastPage}>&gt;&gt;</button>:null):null}
+          </div>
+          </div>
         
         
-        <div className="container-menu">
-          {recData.item?<button className="pageButton" onClick={previousPage}> &lt;</button>:null}
-          {recData.item?<button className= "pageButton" onClick={nextPage}>&gt;</button>:null}
-        </div>
-          <br/>
-          {recData.item?<SortBy onChange={handleChange} />:null}
-          <br/>
 
-          {recData.item ? (recData.item.length>0? recData.item.slice(menuPage*10,menuPage*10+10).map(createList):"No list available") : "Select your option"}
-        
       </div>
     
+
       <Footer />
     </div>
 
