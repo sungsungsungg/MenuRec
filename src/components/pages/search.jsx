dc15f2636a22218ff51d62050251e63086933b51
Sunghoon Kang <sunghoon@Sunghoons-Air.cable.rcn.com>
2025-04-02 01:49:55 -0400

    Delete errorr handling

--- src/components/pages/search.jsx
+++ src/components/pages/search.jsx
@@ -139,7 +139,7 @@ function SearchPage({ recData, handleSubmit, handleChange, coordinates, selected
   }
 
   function firstPage(){
-    console.log("resetting to the first page");
+    // console.log("resetting to the first page");
     setMenuPage(0);
   }
 
